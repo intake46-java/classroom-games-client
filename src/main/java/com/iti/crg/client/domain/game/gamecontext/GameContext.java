af45664a2b679661ad2848f571cb7964c360f4a4
Abdelrahman Rashed <abdelrahman.rashed.ali@gmail.com>
2026-01-06 18:36:52 +0200

    merging all implemented classes

--- src/main/java/com/iti/crg/client/domain/game/gamecontext/GameContext.java
+++ src/main/java/com/iti/crg/client/domain/game/gamecontext/GameContext.java
@@ -1,5 +1,29 @@
 package com.iti.crg.client.domain.game.gamecontext;
 
+import com.iti.crg.client.domain.game.gamehandling.GameHandling;
+
 public abstract class GameContext {
-    
+
+    // Changed from 'TicTacToeGame' to the interface 'GameHandling'
+    protected GameHandling game;
+
+    // Dependency Injection: Pass the specific game implementation here
+    public GameContext(GameHandling game) {
+        this.game = game;
+        this.game.startGame();
+    }
+
+    public GameHandling getGame() {
+        return game;
+    }
+
+    // Abstract method remains the same
+    public abstract void processMove(int row, int col, GameCallback callback);
+
+    // Interface to communicate back to the UI Controller
+    public interface GameCallback {
+        void onMoveMade(int row, int col, char symbol);
+        void onGameWin(char winner);
+        void onGameTie();
+    }
 }
--- src/main/java/com/iti/crg/client/domain/game/gamehandling/TicTacToeGame.java
+++ src/main/java/com/iti/crg/client/domain/game/gamehandling/TicTacToeGame.java
@@ -1,19 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.iti.crg.client.domain.game.gamehandling;
 
 import com.iti.crg.client.domain.entities.Cell;
 import com.iti.crg.client.domain.entities.TicTacToeBoard;
 
-/**
- *
- * @author dell
- */
 public class TicTacToeGame extends TicTacToeBoard implements GameHandling {
 
+    private char currentPlayer;
+    private boolean isGameOver;
+
+    public TicTacToeGame() {
+        super();
+        startGame();
+    }
+
+    // --- Implementing the new GameHandling methods ---
+
     @Override
+    public boolean makeMove(int row, int col) {
+        if (isGameOver || !isValidMove(new Cell(row, col))) {
+            return false;
+        }
+        grid[row][col] = currentPlayer;
+        return true;
+    }
+
+    @Override
+    public boolean checkWin() {
+        return isWinner(this);
+    }
+
+    @Override
+    public boolean checkTie() {
+        return isTie(this);
+    }
+
+    @Override
+    public boolean isGameOver() {
+        return isGameOver;
+    }
+
+    @Override
+    public char getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    @Override
+    public void startGame() {
+        // Clear board logic
+        for (int r = 0; r < getRows(); r++) {
+            for (int c = 0; c < getCols(); c++) {
+                grid[r][c] = '_';
+            }
+        }
+        currentPlayer = 'X';
+        isGameOver = false;
+    }
+
+    @Override
+    public void endGame() {
+        isGameOver = true;
+    }
+
+    @Override
+    public void changeTurn() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
     public boolean isValidMove(Cell cell) {
         int row = cell.getRow();
         int col = cell.getCol();
@@ -21,23 +73,22 @@ public class TicTacToeGame extends TicTacToeBoard implements GameHandling {
 
         return row >= 0 && row < 3
                 && col >= 0 && col < 3
-                && grid[row][col] == ' ';
+                && grid[row][col] == '_';
     }
 
-    @Override
     public boolean isWinner(TicTacToeBoard board) {
         char[][] grid = board.getGrid();
         for (int i = 0; i < board.getRows(); i++) {
             // Check Rows
-            if (grid[i][0] != ' ' && grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2]) {
+            if (grid[i][0] != '_' && grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2]) {
                 return true;
             } // Check Columns
-            else if (grid[0][i] != ' ' && grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i]) {
+            else if (grid[0][i] != '_' && grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i]) {
                 return true;
             }
         }
         // Check Diagonals
-        if (grid[1][1] != ' ') {
+        if (grid[1][1] != '_') {
             if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]) {
                 return true;
             } else if (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0]) {
@@ -48,7 +99,6 @@ public class TicTacToeGame extends TicTacToeBoard implements GameHandling {
         return false;
     }
 
-    @Override
     public boolean isTie(TicTacToeBoard board) {
         return !isWinner(board) && board.isFull();
     }
