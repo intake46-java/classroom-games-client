a03a863720ef69b06507e58a68ffe7984a80dafe
Abdelrahman Rashed <abdelrahman.rashed.ali@gmail.com>
2026-01-12 01:46:42 +0200

    add online playing

---
+++ src/main/java/com/iti/crg/client/domain/repository/GameRepository.java
@@ -0,0 +1,9 @@
+package com.iti.crg.client.domain.repository;
+
+import com.iti.crg.client.infrastructure.dto.GameMoveDto;
+
+public interface GameRepository {
+    boolean sendMove(GameMoveDto moveDto);
+    void sendWin(String opponent); // Notify server/opponent of end game
+    void sendTie(String opponent);
+}
