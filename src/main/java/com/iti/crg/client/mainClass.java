ce09612b6a6fc8fbafd9eeace6521d8a7bf0e61b
Osama Khaled <osamakhaled31415@gmail.com>
2026-01-14 12:31:53 +0200

    jars

---
+++ src/main/java/com/iti/crg/client/mainClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.iti.crg.client;
+
+/**
+ *
+ * @author Osama
+ */
+public class mainClass {
+    public static void main(String[] args) {
+       App.main(args);
+    }
+}
